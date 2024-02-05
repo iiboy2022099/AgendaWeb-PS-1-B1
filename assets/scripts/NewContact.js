81829c6deeda7c01d4e4f29a8b5ef08703de3a51
Ignacio Iboy <iiboy-2022099@kinal.edu.gt>
2024-02-04 22:43:46 -0600

    Subiendo New Contactos

--- assets/scripts/NewContact.js
+++ assets/scripts/NewContact.js
@@ -2,11 +2,13 @@ function agregarContacto() {
     var nombre = document.getElementById('nombre').value;
     var telefono = document.getElementById('telefono').value;
     var email = document.getElementById('email').value;
+    var carrera = document.getElementById('carrera').value;
 
     var contactoElement = document.createElement('p');
-    contactoElement.innerHTML = "<strong>Nombre:</strong> " + nombre + ", <strong>Teléfono:</strong> " + telefono + ", <strong>Email:</strong> " + email;
+    contactoElement.innerHTML = "<strong>Nombre:</strong> " + nombre + ", <strong>Teléfono:</strong> " + telefono + ", <strong>Email:</strong> " + email+", <strong>Carrera:</strong> " + carrera;
 
     document.getElementById('contactList').appendChild(contactoElement);
 
     document.getElementById('contactForm').reset();
+    
 }
